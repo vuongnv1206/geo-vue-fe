69179eb7a89b7990f06e2700f2c6e750d8e8d3bc
DFGDF234 <kynvche163260@fpt.edu.vn>
2024-05-26 02:46:02 +0700

    chore: Update API base URL and add reCAPTCHA support

--- src/pages/auth/RecoverPassword.vue
+++ src/pages/auth/RecoverPassword.vue
@@ -5,13 +5,7 @@
       If you've forgotten your password, don't worry. Simply enter your email address below, and we'll send you an email
       with a temporary password. Restoring access to your account has never been easier.
     </p>
-    <VaInput
-      v-model="email"
-      :rules="[(v) => !!v || 'Email field is required']"
-      class="mb-4"
-      label="Enter your email"
-      type="email"
-    />
+    <VaInput v-model="email" :rules="emailRules" class="mb-4" label="Enter your email" type="email" />
     <VaButton class="w-full mb-2" @click="submit">Send password</VaButton>
     <VaButton :to="{ name: 'login' }" class="w-full" preset="secondary" @click="submit">Go back</VaButton>
   </VaForm>
@@ -21,14 +15,23 @@
 import { ref } from 'vue'
 import { useForm } from 'vuestic-ui'
 import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/modules/auth.module'
 
 const email = ref('')
 const form = useForm('passwordForm')
 const router = useRouter()
+const store = useAuthStore()
+
+const emailRules: ((v: string) => boolean | string)[] = [
+  (v) => !!v || 'Email field is required',
+  (v) => /.+@.+\..+/.test(v) || 'Email should be valid',
+]
 
 const submit = () => {
   if (form.validate()) {
-    router.push({ name: 'recover-password-email' })
+    store.forgotPassword(email.value).finally(() => {
+      router.push({ name: 'recover-password-email' })
+    })
   }
 }
 </script>
