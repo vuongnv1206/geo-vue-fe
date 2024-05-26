de6783ac5dd55ebcc059d02a6f18401c36cdd559
DFGDF234 <kynvche163260@fpt.edu.vn>
2024-05-26 12:31:42 +0700

    chore: Add loading state to email confirmation and password recovery pages

--- src/pages/auth/ConfirmEmail.vue
+++ src/pages/auth/ConfirmEmail.vue
@@ -1,24 +1,26 @@
 <template>
   <div>
-    <h1 v-if="isConfirmed" class="font-semibold text-4xl mb-4">Success confirmation</h1>
-    <h1 v-else class="font-semibold text-4xl mb-4">Failed confirmation</h1>
+    <VaInnerLoading :loading="isLoading" :size="60">
+      <h1 v-if="isConfirmed" class="font-semibold text-4xl mb-4">Success confirmation</h1>
+      <h1 v-else class="font-semibold text-4xl mb-4">Failed confirmation</h1>
 
-    <!-- Icon -->
-    <div class="flex justify-center">
-      <VaIcon v-if="isConfirmed" class="text-6xl text-green-500" name="check_circle" size="4rem" />
-      <VaIcon v-else class="text-6xl text-red-500" name="close" size="4rem" />
-    </div>
+      <!-- Icon -->
+      <div class="flex justify-center">
+        <VaIcon v-if="isConfirmed" class="text-6xl text-green-500" name="check_circle" size="4rem" />
+        <VaIcon v-else class="text-6xl text-red-500" name="close" size="4rem" />
+      </div>
 
-    <p v-if="isConfirmed" class="text-base mb-4 leading-5">
-      Your email has been confirmed. You can now log in to your account.
-    </p>
-    <p v-else class="text-base mb-4 leading-5">
-      Your email could not be confirmed. Please try again or contact support.
-    </p>
+      <p v-if="isConfirmed" class="text-base mb-4 leading-5">
+        Your email has been confirmed. You can now log in to your account.
+      </p>
+      <p v-else class="text-base mb-4 leading-5">
+        Your email could not be confirmed. Please try again or contact support.
+      </p>
 
-    <div class="flex justify-center mt-4">
-      <VaButton :to="{ name: 'login' }" class="w-full">Back to login</VaButton>
-    </div>
+      <div class="flex justify-center mt-4">
+        <VaButton :to="{ name: 'login' }" class="w-full">Back to login</VaButton>
+      </div>
+    </VaInnerLoading>
   </div>
 </template>
 
@@ -36,6 +38,7 @@ const code = route.query.code as string
 const tenant = route.query.tenant as string
 
 const isConfirmed = ref(false)
+const isLoading = ref(true)
 
 store
   .confirmEmail(tenant, userId, code)
@@ -44,5 +47,8 @@ store
   })
   .catch(() => {
     isConfirmed.value = false
+  })
+  .finally(() => {
+    isLoading.value = false
   })
 </script>
