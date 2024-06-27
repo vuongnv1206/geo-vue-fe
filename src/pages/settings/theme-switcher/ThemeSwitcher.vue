9d010179175044fa8656337240f58d1b42b17825
nguyenvancaokyfpt <kynvche163260@fpt.edu.vn>
2024-06-27 15:34:56 +0700

    refactor: Update theme switcher component
    This commit updates the ThemeSwitcher.vue component to use the VaSwitch component instead of VaButtonToggle. It improves the user interface by providing a more visually appealing and intuitive switch for toggling between light and dark themes.

--- src/pages/settings/theme-switcher/ThemeSwitcher.vue
+++ src/pages/settings/theme-switcher/ThemeSwitcher.vue
@@ -1,14 +1,27 @@
 <template>
-  <VaButtonToggle v-model="theme" color="background-element" border-color="background-element" :options="options" />
+  <VaSwitch
+    v-model="mode"
+    class="mr-5"
+    color="#5123a1"
+    off-color="#ffd300"
+    size="small"
+    style="--va-switch-checker-background-color: #252723"
+  >
+    <template #innerLabel>
+      <div class="va-text-center">
+        <VaIcon :name="mode ? 'dark_mode' : 'light_mode'" />
+      </div>
+    </template>
+  </VaSwitch>
 </template>
 <script lang="ts" setup>
-import { computed, onMounted } from 'vue'
-
-import { useI18n } from 'vue-i18n'
+import { computed, onMounted, ref, watch } from 'vue'
 
 import { useColors } from 'vuestic-ui'
 
 const { applyPreset, currentPresetName } = useColors()
+
+const mode = ref(0)
 
 const theme = computed({
   get() {
@@ -20,12 +33,12 @@ const theme = computed({
   },
 })
 
-const { t } = useI18n()
-
-const options = [
-  { label: t('buttonSelect.dark'), value: 'dark' },
-  { label: t('buttonSelect.light'), value: 'light' },
-]
+watch(
+  () => mode.value,
+  (value) => {
+    theme.value = value ? 'dark' : 'light'
+  },
+)
 
 onMounted(() => {
   const theme = localStorage.getItem('theme')
