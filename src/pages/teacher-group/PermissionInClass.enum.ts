709c306a0b0964143407ddce13df859f0a37fd29
thang1834 <90250933+thang1834@users.noreply.github.com>
2024-07-18 05:06:06 +0700

    refactor: Update class permission translations in PermissionInClass.enum.ts

--- src/pages/teacher-group/PermissionInClass.enum.ts
+++ src/pages/teacher-group/PermissionInClass.enum.ts
@@ -1,5 +1,8 @@
+import i18n from '../../i18n'
+const { t } = i18n.global
+
 export const PermissionNameInClass = {
-  AssignAssignment: 'Assign Assignment',
-  Marking: 'Marking',
-  ManageStudentList: 'Manage Student List',
+  AssignAssignment: t('class_permission.assign_assignment'),
+  Marking: t('class_permission.marking'),
+  ManageStudentList: t('class_permission.manage_students'),
 }
