5d534b8477f20e8adeb3d97fbd55fb474ec59d92
thang1834 <90250933+thang1834@users.noreply.github.com>
2024-08-24 15:57:31 +0700

    fix: Remove commented out code for getting assignment submissions

--- src/pages/assignment/widgets/SubmitAssignment.vue
+++ src/pages/assignment/widgets/SubmitAssignment.vue
@@ -93,7 +93,8 @@ const getAssignmentSubmissions = () => {
     })
 }
 
-const fileUpload = async () => {
+const fileUpload = () => {
+  loading.value = true
   fileStore
     .uploadFile(filesUploaded.value)
     .then((response) => {
@@ -105,6 +106,9 @@ const fileUpload = async () => {
         message: notifications.uploadFailed + getErrorMessage(error),
         color: 'error',
       })
+    })
+    .finally(() => {
+      loading.value = false
     })
 }
 
@@ -178,62 +182,64 @@ onMounted(() => {
     </VaCardContent>
     <VaDivider />
     <VaCardTitle>{{ $t('assignments.submit_homeworks') }}</VaCardTitle>
-    <VaCard v-if="assignmentSubmissions[0]?.score == null">
-      <VaForm>
-        <div
-          v-if="new Date() >= new Date(assignment.startTime) && new Date() <= new Date(assignment.endTime)"
-          class="px-3.5"
-        >
-          <QuillEditor
-            v-model:content="newAssignmentSubmit.answerRaw"
-            class="border rounded"
-            :placeholder="$t('posts.enter_content')"
-            content-type="html"
-            style="height: 200px"
-          />
-          <VaCardContent class="flex flex-col items-center">
-            <VaFileUpload
-              v-model="filesUploaded"
-              dropzone
-              :upload-button-text="$t('file_upload.upload_button_text')"
-              :drop-zone-text="$t('file_upload.drop_zone_text')"
-              file-types="jpg,png,jpeg,pdf,doc,docx,xls,xlsx,ppt,pptx,txt,zip,rar,7z,mp4,avi,mkv,
-              flv,wmv,mov,webm,mp3,wav,flac,ogg,wma,json,xml,csv,tsv"
-              class="border-2 border-dashed border-gray-300 flex items-center justify-center w-full py-6"
-              @fileAdded="fileUpload"
+    <VaInnerLoading :loading="loading">
+      <VaCard v-if="assignmentSubmissions[0]?.score == null">
+        <VaForm>
+          <div
+            v-if="new Date() >= new Date(assignment.startTime) && new Date() <= new Date(assignment.endTime)"
+            class="px-3.5"
+          >
+            <QuillEditor
+              v-model:content="newAssignmentSubmit.answerRaw"
+              class="border rounded"
+              :placeholder="$t('posts.enter_content')"
+              content-type="html"
+              style="height: 200px"
             />
-            <VaButton class="mt-4" @click="onAssignmentSubmit">{{ $t('settings.submit') }}</VaButton>
-          </VaCardContent>
-        </div>
-        <div v-else class="text-center text-red-500 border-2 border-dashed rounded p-4 mx-3">
-          {{ $t('assignments.submission_time_over') }}
-        </div>
-      </VaForm>
-    </VaCard>
-    <VaCard v-else>
-      <VaCard v-if="!assignment.canViewResult">
-        <VaCardContent class="flex flex-col items-center">
-          <VaIcon name="done" size="large" class="material-symbols-outlined mb-2" />
-          <p>{{ $t('assignments.submitted') }}</p>
-        </VaCardContent>
+            <VaCardContent class="flex flex-col items-center">
+              <VaFileUpload
+                v-model="filesUploaded"
+                dropzone
+                :upload-button-text="$t('file_upload.upload_button_text')"
+                :drop-zone-text="$t('file_upload.drop_zone_text')"
+                file-types="jpg,png,jpeg,pdf,doc,docx,xls,xlsx,ppt,pptx,txt,zip,rar,7z,mp4,avi,mkv,
+              flv,wmv,mov,webm,mp3,wav,flac,ogg,wma,json,xml,csv,tsv"
+                class="border-2 border-dashed border-gray-300 flex items-center justify-center w-full py-6"
+                @fileAdded="fileUpload"
+              />
+              <VaButton class="mt-4" @click="onAssignmentSubmit">{{ $t('settings.submit') }}</VaButton>
+            </VaCardContent>
+          </div>
+          <div v-else class="text-center text-red-500 border-2 border-dashed rounded p-4 mx-3">
+            {{ $t('assignments.submission_time_over') }}
+          </div>
+        </VaForm>
       </VaCard>
       <VaCard v-else>
-        <VaCardContent class="font-semibold mr-1">
-          {{ $t('assignments.score') }}:
-          <span class="text-red-500">{{ assignmentSubmissions[0]?.score }}</span>
-        </VaCardContent>
-        <VaCardContent class="font-semibold mr-1">
-          {{ $t('assignments.comment') }}:
-          <VaScrollContainer class="max-h-[48vh]">
-            <!-- eslint-disable vue/no-v-html -->
-            <div
-              class="text-md font-medium px-5 mt-2 mb-4 max-h-48 overflow-y-auto break-words whitespace-pre-line"
-              v-html="assignmentSubmissions[0]?.comment"
-            ></div>
-            <!-- eslint-enable -->
-          </VaScrollContainer>
-        </VaCardContent>
+        <VaCard v-if="!assignment.canViewResult">
+          <VaCardContent class="flex flex-col items-center">
+            <VaIcon name="done" size="large" class="material-symbols-outlined mb-2" />
+            <p>{{ $t('assignments.submitted') }}</p>
+          </VaCardContent>
+        </VaCard>
+        <VaCard v-else>
+          <VaCardContent class="font-semibold mr-1">
+            {{ $t('assignments.score') }}:
+            <span class="text-red-500">{{ assignmentSubmissions[0]?.score }}</span>
+          </VaCardContent>
+          <VaCardContent class="font-semibold mr-1">
+            {{ $t('assignments.comment') }}:
+            <VaScrollContainer class="max-h-[48vh]">
+              <!-- eslint-disable vue/no-v-html -->
+              <div
+                class="text-md font-medium px-5 mt-2 mb-4 max-h-48 overflow-y-auto break-words whitespace-pre-line"
+                v-html="assignmentSubmissions[0]?.comment"
+              ></div>
+              <!-- eslint-enable -->
+            </VaScrollContainer>
+          </VaCardContent>
+        </VaCard>
       </VaCard>
-    </VaCard>
+    </VaInnerLoading>
   </VaCard>
 </template>
